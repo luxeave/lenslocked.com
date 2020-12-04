fdd74572cc195c5dd990fd6c3df6b175e2cc3045
Stephen Antoni <luxeaves@gmail.com>
2020-12-05 00:10:53 +0700

    Created out first web server!

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func someHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", someHandlerFunc)
+	http.ListenAndServe(":3000", nil) // :3000 or localhost:3000 the same
 }
