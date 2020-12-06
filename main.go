f57596ffa2c67cacdf4b3bbf2a71b0fc0c491f18
Stephen Antoni <luxeaves@gmail.com>
2020-12-06 22:09:56 +0700

    Added a contact page

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 func someHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my bad site</h1>")
+	//fmt.Fprint(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <h href=\"mailtoLsupport@lenslocked.com\">support@lenslocked.com</a>.")
+	}
+
 }
 
 func main() {
